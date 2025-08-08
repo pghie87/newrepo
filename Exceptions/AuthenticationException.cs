148b747c51a6b688ead17870d1ed41470c7eac58
pghie87 <76475961+pghie87@users.noreply.github.com>
2025-08-08 10:55:19 +0700

    Initial upload - Exceptions/AuthenticationException.cs

---
+++ Exceptions/AuthenticationException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace EmsalEWayBillSystem.Exceptions
+{
+    public class AuthenticationException : Exception
+    {
+        public AuthenticationException(string message) : base(message)
+        {
+        }
+        
+        public AuthenticationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
